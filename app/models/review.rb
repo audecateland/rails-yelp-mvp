4aa03a0b5ebd47298cbe92aeddd052e49c6c3c5a
aude cateland <aude.cateland@gmail.com>
2022-11-16 16:48:04 +0100

    day28 02yelp controllers done

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,5 @@ class Review < ApplicationRecord
   validates :content, presence: true
   validates :rating, presence: true
   validates :rating, numericality: { only_integer: true }
+  validates :rating, inclusion: { in: 0..5 }
 end
